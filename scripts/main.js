b8abebbd00a4efcd96153c4928b9e5f57ef36a04
Michael Dillie <michaeldillie@gmail.com>
2015-09-21 19:51:00 -0500

    Added

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function Quarterback(name, team) {
+	this.name = name;
+	this.team = team;
+	this.type = 'Quarterback';
+	this.attempts = 0;
+	this.completions = 0;
+	this.yards = 0;
+	this.touchdowns = 0;
+	this.interceptions = 0;
+	this.sacks = 0;
+	this.longestCompletion = 0;
